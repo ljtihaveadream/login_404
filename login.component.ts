422629ba1dcf43abf1d77709402ce36a9ee41f98
博观约取才能厚积薄发 <35102621+ljtihaveadream@users.noreply.github.com>
2018-07-25 15:25:30 +0800

    Add files via upload

---
+++ login.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.less']
+})
+export class LoginComponent implements OnInit {
+  logoName = '中浩德';
+  ads: string[] = ['全面', '专业', '便捷', '协同'];
+  sysName = '数据门户系统';
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ not-found.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.less']
+})
+export class NotFoundComponent implements OnInit {
+  title = 'Uh oh!';
+  subtitle = '小怪兽断开了页面链接...';
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
